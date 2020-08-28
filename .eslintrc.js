445accc3ef51f0870ce68f632d752e0468429d75
Yury <dayrobis.garcia@musala.com>
2020-08-28 22:49:21 +0300

    chore: add initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,74 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'airbnb',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'jest',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.ts', '.tsx'] }],
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
+      mjs: 'never',
+    }],
+
+    // jest rules
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    'jest/prefer-expect-assertions': 'off',
+
+    // react rules
+    'react/button-has-type': 'off',
+    'react/prop-types': 'off',
+    'react/destructuring-assignment': ['off', 'never'],
+    'react/state-in-constructor': ['off', 'never'],
+    'react/jsx-one-expression-per-line': ['off'],
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+        ],
+      },
+    },
+  },
+};
