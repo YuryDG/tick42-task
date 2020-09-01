af1a45b9edc313a536659f50357e0fa96c813882
Yury <dayrobis.garcia@musala.com>
2020-09-02 02:07:19 +0300

    feat(employees): add manage unemployment component

---
+++ src/modules/employees/components/ManageUnemployment.tsx
@@ -0,0 +1,110 @@
+/* eslint-disable react/require-default-props */
+import React, { useState } from 'react';
+import MaterialTable from 'material-table';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+import { Employee } from '../types';
+import { Project } from '../../projects/types';
+
+type Props = {
+  employees: Employee[],
+  projects: Project[],
+  onAssignEmployees: (project: Project) => void
+}
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  employeesTable: {
+    backgroundColor: '#cfe8fc',
+    padding: 20,
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    width: 120,
+    marginBottom: 30,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  select: {
+    width: 300,
+  },
+}));
+
+const ManageUnemployment = ({ employees, projects, onAssignEmployees }: Props): JSX.Element => {
+  const classes = useStyles();
+  const [currentProjectId, setCurrentProjectId] = useState('');
+  const [title, setTitle] = useState('');
+
+  const handleAssignToProject = (event: any, employee: Employee | Employee[]) => {
+    const currentProject = projects.find((project) => project.id === currentProjectId);
+    if (currentProject) {
+      if (Array.isArray(employee)) {
+        const ids = employee.map((emp) => emp.id);
+        currentProject.employees.push(...ids);
+      } else {
+        currentProject.employees.push(employee.id);
+      }
+      onAssignEmployees(currentProject);
+    } else {
+      alert('should select a Project');
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    const projectId = event.target.value as string;
+    setCurrentProjectId(projectId);
+    const selectedProject = projects.find((project) => project.id === projectId);
+    if (selectedProject) {
+      setTitle(`Project: ${selectedProject.name}`);
+    }
+  };
+
+  return (
+    <>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="project-select-label">Project name</InputLabel>
+        <Select
+          className={classes.select}
+          labelId="project-select-label"
+          id="project-select"
+          value={currentProjectId}
+          onChange={handleChange}
+        >
+          {
+            projects.map((project) => <MenuItem value={project.id}>{project.name}</MenuItem>)
+          }
+        </Select>
+      </FormControl>
+
+      <MaterialTable<Employee>
+        title={title}
+        columns={[
+          { title: 'Name', field: 'firstName' },
+          { title: 'Last name', field: 'lastName' },
+          { title: 'DateOfBirth', field: 'dateOfBirth', type: 'date' },
+          { title: 'Job Title', field: 'jobTitle' },
+          { title: 'Job Area', field: 'jobArea' },
+          { title: 'Job Type', field: 'jobType' },
+        ]}
+        data={employees}
+        options={{
+          paging: false,
+          selection: true,
+        }}
+        actions={[
+          {
+            tooltip: 'Assign to selected project',
+            icon: 'group_add',
+            onClick: handleAssignToProject,
+          },
+        ]}
+      />
+    </>
+  );
+};
+
+export default ManageUnemployment;
