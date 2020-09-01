8ba2667ec3961fb4bcd1e7e08037212a9d68171e
Yury <dayrobis.garcia@musala.com>
2020-09-02 00:16:29 +0300

    feat(app): add tab-panel component

---
+++ src/modules/app/components/TapPanel.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
+interface TabPanelProps {
+  index: number;
+  value: number;
+  children: React.ReactNode
+}
+
+const TabPanel = (props: TabPanelProps): JSX.Element => {
+  const { value, index, children } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+export default TabPanel;
