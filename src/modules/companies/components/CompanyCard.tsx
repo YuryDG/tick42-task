f8ada208d20bf1404b91af360fd932c5825586c8
Yury <dayrobis.garcia@musala.com>
2020-09-02 02:09:25 +0300

    feat(companies): add company card component

---
+++ src/modules/companies/components/CompanyCard.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { Company } from '../types';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+type Props = {
+  company: Company
+}
+
+const CompanyCard = ({ company }: Props): JSX.Element => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          {company.name} - {company.business}
+        </Typography>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          {company.slogan}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CompanyCard;
