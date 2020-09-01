17aa73012efe67ae475746876186369d75da4185
Yury <dayrobis.garcia@musala.com>
2020-09-02 02:16:45 +0300

    feat(app): add App component connected to redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable max-len */
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { AppState } from './store/AppStore';
+import { buildTreeData } from './utils';
+import { SelectedItem } from './modules/app/types';
+import { fetchInitialData } from './modules/companies/actions';
+import { loadDataForCurrentItem } from './modules/app/actions';
+import AppLayout from './modules/app/components/AppLayout';
 
-export default App;
+const mapStateToProps = (state: AppState) => {
+  const { companies, employees, selectedItem } = state;
+  const treeData = buildTreeData(companies, employees);
+  return { treeData, selectedItem };
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const mapDispatchToProps = (dispatch: any) => ({
+  loadCompanies: () => dispatch(fetchInitialData()),
+  loadDataForCurrentNode: (selectedItem: SelectedItem) => dispatch(loadDataForCurrentItem(selectedItem)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppLayout);
