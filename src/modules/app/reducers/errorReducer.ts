02b2d82005347f8dce689cad836779348617cc53
Yury <dayrobis.garcia@musala.com>
2020-09-02 00:11:59 +0300

    feat(app): add error-reducer

---
+++ src/modules/app/reducers/errorReducer.ts
@@ -0,0 +1,15 @@
+import { CompanyActions, CompanyActionTypes } from '../../companies/actions';
+
+const errorReducer = (state = '', action: CompanyActionTypes):string => {
+  switch (action.type) {
+    case CompanyActions.ERROR_LOADING_COMPANIES:
+      return action.payload.error;
+    case CompanyActions.COMPANIES_LOADED:
+    case CompanyActions.LOAD_COMPANIES:
+      return '';
+    default:
+      return state;
+  }
+};
+
+export default errorReducer;
