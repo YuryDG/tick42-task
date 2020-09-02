39a673e225bf34149145e30d4a03bf1a0868d700
Yury <dayrobis.garcia@musala.com>
2020-09-03 02:25:06 +0300

    test(utils): add unit test

--- src/utils/__test__/index.spec.ts
+++ src/utils/__test__/index.spec.ts
@@ -1,5 +1,9 @@
-import { companies, employees, projects } from '../../test-data';
-import { getEmployeesFromCompany, getProjectsFromCompany, getEmployeesJobArea } from '..';
+import {
+  companies, employees, projects, addresses, addressesIndexed, companiesIndexed, employeesIndexed,
+} from '../../test-data';
+import {
+  getEmployeesFromCompany, getProjectsFromCompany, getEmployeesJobArea, getAddressFromCompany, buildTreeData,
+} from '..';
 
 describe('utils', () => {
   it('function getEmployeesFromCompany', () => {
@@ -22,5 +26,57 @@ describe('utils', () => {
     const expectResult = ['Web', 'Implementation', 'Metrics'];
     const result = getEmployeesJobArea(employees);
     expect(result).toStrictEqual(expectResult);
+  });
+
+  it('function getAddressFromCompany', () => {
+    const expectedAddress = addresses[0];
+    const company = companies[0];
+
+    const result = getAddressFromCompany(company, addressesIndexed);
+    expect(result).toStrictEqual(expectedAddress);
+  });
+
+  it('function buildTreeData', () => {
+    const expectedTreeData = {
+      items: [
+        {
+          company: {
+            business: 'world-class streamline supply-chains',
+            id: '1',
+            name: 'Hackett - Greenfelder',
+            slogan: 'Profit-focused holistic info-mediaries',
+          },
+          jobAreas: [
+            {
+              areaId: 'Web*1',
+              name: 'Web',
+              employees: [
+                {
+                  companyId: '1',
+                  dateOfBirth: '1965-01-10T20:48:37.953Z',
+                  firstName: 'Fanny',
+                  id: '1',
+                  jobArea: 'Web',
+                  jobTitle: 'District Web Technician',
+                  jobType: 'Manager',
+                  lastName: 'Crona',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+
+    const myCompanies = {
+      1: companies[0],
+    };
+
+    const myEmployees = {
+      1: employees[0],
+    };
+
+    const result = buildTreeData(myCompanies, myEmployees);
+    expect(result).toStrictEqual(expectedTreeData);
   });
 });
