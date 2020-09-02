3ca431d44e791f98b2a509c204c4dafb965a0ecf
Yury <dayrobis.garcia@musala.com>
2020-09-02 20:46:56 +0300

    feat(app): update loading reducer

--- src/modules/app/reducers/loadingReducer.ts
+++ src/modules/app/reducers/loadingReducer.ts
@@ -4,9 +4,6 @@ const loadingReducer = (state = false, action: CompanyActionTypes): boolean => {
   switch (action.type) {
     case CompanyActions.LOAD_COMPANIES:
       return true;
-    case CompanyActions.ERROR_LOADING_COMPANIES:
-    case CompanyActions.COMPANIES_LOADED:
-      return false;
     default:
       return state;
   }
