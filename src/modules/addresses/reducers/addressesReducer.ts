edf9489544268d6b8828c15dcd61950438118f46
Yury <dayrobis.garcia@musala.com>
2020-09-02 16:52:03 +0300

    feat(address): add reducer

--- src/modules/addresses/reducers/addressesReducer.ts
+++ src/modules/addresses/reducers/addressesReducer.ts
@@ -2,17 +2,17 @@ import { AddressIndexed } from '../types';
 import { CompanyActions, CompanyActionTypes } from '../../companies/actions';
 
 // eslint-disable-next-line max-len
-const addressesReducer = (state: AddressIndexed = {}, action: CompanyActionTypes):
-AddressIndexed => {
-  console.time('addressesReducer');
+const addressesReducer = (state: AddressIndexed = {}, action: CompanyActionTypes): AddressIndexed => {
   switch (action.type) {
     case CompanyActions.COMPANY_LOADED: {
       const { address } = action.payload;
-      const result = {
-        ...state,
-        [address.id]: address,
-      };
-      console.timeLog('addressesReducer');
+      let result = state;
+      if (address) {
+        result = {
+          ...state,
+          [address.id]: address,
+        };
+      }
       return result;
     }
     default:
