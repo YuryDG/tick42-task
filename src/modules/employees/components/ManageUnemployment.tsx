2d5e3fc3eaf9f6cd7d3f62b73dd03bfb991ceea4
Yury <dayrobis.garcia@musala.com>
2020-09-03 02:26:20 +0300

    feat(employees): show error when network request fails

--- src/modules/employees/components/ManageUnemployment.tsx
+++ src/modules/employees/components/ManageUnemployment.tsx
@@ -5,7 +5,9 @@ import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
+import Alert from '@material-ui/lab/Alert';
 import Select from '@material-ui/core/Select';
+import { Typography } from '@material-ui/core';
 
 import { Employee } from '../types';
 import { Project } from '../../projects/types';
@@ -38,6 +40,7 @@ const ManageUnemployment = ({ employees, projects, onAssignEmployees }: Props): 
   const classes = useStyles();
   const [currentProjectId, setCurrentProjectId] = useState('');
   const [title, setTitle] = useState('');
+  const [error, setError] = useState('');
 
   const handleAssignToProject = (event: any, employee: Employee | Employee[]) => {
     const currentProject = projects.find((project) => project.id === currentProjectId);
@@ -50,13 +53,14 @@ const ManageUnemployment = ({ employees, projects, onAssignEmployees }: Props): 
       }
       onAssignEmployees(currentProject);
     } else {
-      alert('should select a Project');
+      setError('You should select a project');
     }
   };
 
   const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
     const projectId = event.target.value as string;
     setCurrentProjectId(projectId);
+    setError('');
     const selectedProject = projects.find((project) => project.id === projectId);
     if (selectedProject) {
       setTitle(`Project: ${selectedProject.name}`);
@@ -79,6 +83,12 @@ const ManageUnemployment = ({ employees, projects, onAssignEmployees }: Props): 
           }
         </Select>
       </FormControl>
+
+      <Typography component="div">
+        {error && <Alert severity="error">{error}</Alert> }
+      </Typography>
+
+      <br />
 
       <MaterialTable<Employee>
         title={title}
