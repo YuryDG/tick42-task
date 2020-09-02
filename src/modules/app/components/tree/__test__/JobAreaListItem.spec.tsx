550e9409456a496220f44d710dc7277fcb4a1de8
Yury <dayrobis.garcia@musala.com>
2020-09-02 13:54:21 +0300

    test(app): unit test for JobAreaListItem component

--- src/modules/app/components/tree/__test__/JobAreaListItem.spec.tsx
+++ src/modules/app/components/tree/__test__/JobAreaListItem.spec.tsx
@@ -1,27 +1,28 @@
-import React from "react";
-import { shallow } from "enzyme";
-import JobAreaListItem from "../JobAreaListItem";
+/* eslint-disable */ 
+import React from 'react';
+import { shallow } from 'enzyme';
+import JobAreaListItem from '../JobAreaListItem';
 
-import { JobArea } from "../../../types";
-import { employees } from "../../../../../test-data";
-import { Employee } from "../../../../employees/types";
+import { JobArea } from '../../../types';
+import { employees } from '../../../../../test-data';
+import { Employee } from '../../../../employees/types';
 
 const createItem = (data: Employee[]) => ({
-  areaId: "Area #1",
-  name: "Area 1",
+  areaId: 'Area #1',
+  name: 'Area 1',
   employees: data,
 });
 
-describe("app/tree/JobAreaListItem", () => {
-  it("should render properly a job area without employees", () => {
+describe('app/tree/JobAreaListItem', () => {
+  it('should render properly a job area without employees', () => {
     const item: JobArea = createItem([]);
     const wrapper = shallow(<JobAreaListItem item={item} />);
-    expect(wrapper).toMatchInlineSnapshot(`ShallowWrapper {}`);
+    expect(wrapper).toMatchSnapshot();
   });
 
-  it("should render properly a job area with employees", () => {
+  it('should render properly a job area with employees', () => {
     const item: JobArea = createItem(employees);
     const wrapper = shallow(<JobAreaListItem item={item} />);
-    expect(wrapper).toMatchInlineSnapshot(`ShallowWrapper {}`);
+    expect(wrapper).toMatchSnapshot();
   });
 });
