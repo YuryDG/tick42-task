3a0f7391931acc9d37bfd8aa3f4b12c54d42359f
Yury <dayrobis.garcia@musala.com>
2020-09-02 03:23:46 +0300

    test(utils): add unit test

--- src/utils/__test__/index.spec.ts
+++ src/utils/__test__/index.spec.ts
@@ -1,5 +1,26 @@
+import { companies, employees, projects } from '../../test-data';
+import { getEmployeesFromCompany, getProjectsFromCompany, getEmployeesJobArea } from '..';
+
 describe('utils', () => {
-  it('should', () => {
-    expect(2).toStrictEqual(1);
+  it('function getEmployeesFromCompany', () => {
+    const company = companies[0];
+    const expectedIdEmployees = ['1', '2'];
+    const result = getEmployeesFromCompany(company, employees);
+    const ids = result.map((emp) => emp.id);
+    expect(ids).toStrictEqual(expectedIdEmployees);
+  });
+
+  it('function getProjectsFromCompany', () => {
+    const company = companies[0];
+    const expectedIdProjects = ['1'];
+    const result = getProjectsFromCompany(company, projects);
+    const ids = result.map((emp) => emp.id);
+    expect(ids).toStrictEqual(expectedIdProjects);
+  });
+
+  it('function getEmployeesJobArea', () => {
+    const expectResult = ['Web', 'Implementation', 'Metrics'];
+    const result = getEmployeesJobArea(employees);
+    expect(result).toStrictEqual(expectResult);
   });
 });
